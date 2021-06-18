069df78b9842dd34cfc6fad8d58b1dd00d1e3540
Maryam <maryamreis@hotmail.co.uk>
2021-06-18 15:59:51 +0100

    Added heading stars

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function AppHeader() {
     return(
         <header>
-            <h1>Maryam's Favourite Places</h1>
+            <h1>⭐ Maryam's Favourite Places ⭐</h1>
             <p>All places worth a visit!</p>
         </header>
     )
